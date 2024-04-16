482919855c350f57546fd39e98ecd6c5a0b1cadf
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-04-16 09:47:56 +0300

    minor changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,7 @@
 import LogForm from '@/components/Forms/LogForm'
 import Title from '@/components/UI/title/Title'
-import { authOptions } from '@/lib/authOptions'
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
 
 export default async function SignInPage() {
-    const session = await getServerSession(authOptions)
-
-    if (session) redirect('/profile')
     return (
         <div className='mx-auto w-1/2 mt-20'>
             <Title text='Welcome' />
